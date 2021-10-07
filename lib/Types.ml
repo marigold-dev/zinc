30ef3dbc0627bfe9b981cc1230802d71c43fd37f
Andre Popovitch <andre@popovit.ch>
2021-10-07 21:35:56 +0000

    giving up on implicit_transitive_deps false

--- lib/Types.ml
+++ lib/Types.ml
@@ -2,12 +2,29 @@ module Z = struct
 include Z
 let to_yojson x = `String (Z.to_string x)
 let of_yojson = function
-  | `String s -> Ppx_deriving_yojson_runtime.Result.Ok (Z.of_string s) 
-  | _ -> Ppx_deriving_yojson_runtime.Result.Error "JSON string expected" 
+  | `String s -> Result.Ok (Z.of_string s) 
+  | _ -> Result.Error "JSON string expected" 
+  let pp : Format.formatter -> t -> unit = fun fmt v -> Format.fprintf fmt "%s" (Z.to_string v)
 end
 
 
 type label = Label of string [@@deriving show { with_path = false }, eq, yojson]
+module LMap = struct 
+  include Map.Make(struct type t = label let compare = compare end)
+  type 'a association_list = (label * 'a) list [@@deriving yojson]
+  let of_list (lst: 'a association_list) : 'a t =
+    let aux prev (k, v) = add k v prev in
+    List.fold_left aux empty lst
+  let pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit = fun value ppf m -> 
+    let lst = bindings m in
+    let lst = Base.List.dedup_and_sort ~compare:(fun (Label a,_) (Label b,_) -> String.compare a b) lst in
+    let new_pp ppf (k, v) = Format.fprintf ppf "@[<h>%a -> %a@]" (pp_label) k value v in
+    Format.fprintf ppf "%a" (Format.pp_print_list ?pp_sep:(Some (fun fmt _ -> Format.pp_print_string fmt ", ")) new_pp) lst
+    
+  let of_yojson a m = Result.map of_list (association_list_of_yojson a m) 
+  let to_yojson a m = bindings m |> association_list_to_yojson a
+end
+
 
 type zinc_instruction =
   (* ====================
@@ -34,9 +51,7 @@ type zinc_instruction =
       (label)
   (* math *)
   | Num of
-      (Z.t
-      [@printer fun fmt v -> fprintf fmt "%s" (Z.to_string v)])
-  (*
+      (Z.t)
   | Add
   (* boolean *)
   | Bool of bool
@@ -64,7 +79,6 @@ type zinc_instruction =
   | ChainID
   (* Random handling stuff (need to find a better way to do that) *)
   | Done
-  *)
 [@@deriving show { with_path = false }, eq, yojson]
 
 and zinc = zinc_instruction list
@@ -72,15 +86,13 @@ and zinc = zinc_instruction list
 
 type program = (string * zinc) list
 [@@deriving show { with_path = false }, eq, yojson]
-(*
+
 
 type env_item =
   [ `Z of zinc_instruction
   | `Clos of clos
   | `Record of
-    (stack_item label_map
-    [@printer label_map_printer fprintf pp_stack_item]
-    [@equal Stage_common_types.Types.LMap.equal equal_stack_item]) ] 
+    (stack_item LMap.t) ] 
 [@@deriving show, eq, yojson]
 
 and stack_item =
@@ -88,9 +100,7 @@ and stack_item =
     `Z of zinc_instruction
   | `Clos of clos
   | `Record of
-    (stack_item label_map
-    [@printer label_map_printer fprintf pp_stack_item]
-    [@equal Stage_common_types.Types.LMap.equal equal_stack_item])
+    (stack_item LMap.t)
   | (* marker to note function calls *)
     `Marker of zinc * env_item list ]
 [@@deriving show, eq]
@@ -103,4 +113,3 @@ type env = env_item list [@@deriving show, eq, yojson]
 type stack = stack_item list [@@deriving show, eq, yojson]
 
 type zinc_state = zinc * env * stack  [@@deriving show, eq, yojson]
-*)
